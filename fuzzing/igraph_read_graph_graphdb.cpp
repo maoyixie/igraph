af0e6cb2788b55348d161133bf66e59e36336a1f
maoyixie <maoyi001@e.ntu.edu.sg>
2024-04-14 19:09:18 +0800

    update

--- fuzzing/igraph_read_graph_graphdb.cpp
+++ fuzzing/igraph_read_graph_graphdb.cpp
@@ -1,5 +1,33 @@
 #include <igraph.h>
 #include <cstdio>
+// many undeclared identifiers
+/* ./fuzzing/igraph_read_graph_graphdb.cpp:16:9: error: use of undeclared identifier 'write'; did you mean '_IO_cookie_io_functions_t::write'?
+    if (write(fd, Data, Size) != Size)
+        ^~~~~
+        _IO_cookie_io_functions_t::write
+/usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h:58:28: note: '_IO_cookie_io_functions_t::write' declared here
+  cookie_write_function_t *write;
+                           ^
+/fuzzing/igraph_read_graph_graphdb.cpp:16:9: error: invalid use of non-static data member 'write'
+    if (write(fd, Data, Size) != Size)
+        ^~~~~
+./fuzzing/igraph_read_graph_graphdb.cpp:19:9: error: use of undeclared identifier 'close'
+        close(fd);
+        ^
+./fuzzing/igraph_read_graph_graphdb.cpp:20:9: error: use of undeclared identifier 'unlink'
+        unlink(filename);
+        ^
+./fuzzing/igraph_read_graph_graphdb.cpp:25:5: error: use of undeclared identifier 'close'
+    close(fd);
+    ^
+./fuzzing/igraph_read_graph_graphdb.cpp:32:9: error: use of undeclared identifier 'unlink'
+        unlink(filename);
+        ^
+./fuzzing/igraph_read_graph_graphdb.cpp:46:5: error: use of undeclared identifier 'unlink'
+    unlink(filename);
+    ^
+7 errors generated.
+ERROR:__main__:Building fuzzers failed. */
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
